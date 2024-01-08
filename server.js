cfc8dc5c9d8b5a68ea134f2bca52a39a146630fb
alisahibmammadov <ali.yek.10@mail.ru>
2024-01-08 22:01:53 +0400

    MongoDb installation and connect

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
-
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const port = 5000;
 
-app.get("/",(req,res)=>{
-    res.send("Hello world")
-})
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
 app.listen(port, () => {
+  connect();
   console.log(`Example app listening on port: ${port}`);
 });
