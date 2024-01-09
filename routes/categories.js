f3b1d41bc2c7dc75e2ddcc1a7b1056cf70299008
alisahibmammadov <ali.yek.10@mail.ru>
2024-01-09 13:15:50 +0400

    Category CRUD operations

--- routes/categories.js
+++ routes/categories.js
@@ -2,6 +2,15 @@ const Category = require("../models/Category");
 const express = require("express");
 const router = express.Router();
 
+router.get("/get-all", async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).send(categories);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
 router.post("/add-category", async (req, res) => {
   try {
     const newCategory = new Category(req.body);
@@ -9,6 +18,24 @@ router.post("/add-category", async (req, res) => {
     res.status(200).json("Item added successfully");
   } catch (error) {
     res.status(400).json(error);
+  }
+});
+
+router.put("/update-category/", async (req, res) => {
+  try {
+    await Category.findOneAndUpdate({ _id: req.body._id }, req.body);
+    res.status(200).json("Item updated successfully");
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.delete("/delete-category", async (req, res) => {
+  try {
+    await Category.findOneAndDelete({ _id: req.body._id }, req.body);
+    res.status(200).json("Item deleted successfully");
+  } catch (error) {
+    res.status(400).send(error);
   }
 });
 
