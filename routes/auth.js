cd829c17657290f8366d639d40d729b3e239df0a
alisahibmammadov <ali.yek.10@mail.ru>
2024-01-09 14:34:23 +0400

    Created secure password

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,22 @@
 const User = require("../models/User");
 const express = require("express");
 const router = express.Router();
-
+const bcrypt = require("bcryptjs");
 router.post("/register", async (req, res) => {
   try {
-    const newUser = new User(req.body);
+    const { username, email, password } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
     await newUser.save();
     res.status(200).json("A new user created successfully");
   } catch (error) {
     res.status(400).json(error);
   }
 });
-
-
 
 module.exports = router;
